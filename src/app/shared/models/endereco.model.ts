7635f572a972b9aa17f4f4f6db79a625769977a2
Rodrigo Balaban Zbytkowski <rodrigo.balaban@gmail.com>
2021-08-21 10:19:54 -0300

    Implementa checkbox de "endereço resiencial"

--- src/app/shared/models/endereco.model.ts
+++ src/app/shared/models/endereco.model.ts
@@ -8,6 +8,7 @@ export class Endereco {
     public bairro?: string,
     public cep?: string,
     public cidade?: string,
-    public estado?: string
+    public estado?: string,
+    public residencial?: boolean
   ) {}
 }
